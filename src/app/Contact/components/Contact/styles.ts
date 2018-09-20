091762a90912cce2166246c67ac502f33c13e7dc
severjason <ssever2010@gmail.com>
2018-09-20 11:01:37 +0300

    updated styles

--- src/app/Contact/components/Contact/styles.ts
+++ src/app/Contact/components/Contact/styles.ts
@@ -3,9 +3,6 @@ import styled from 'styled-components';
 const ContactStyles = styled.div`
   margin: .5rem 0;
   
-  .contact-header {
-    width: 100%;
-  }
   .card-action {
     width: 100%;
   }
